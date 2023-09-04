f416eaf7fc82b691ca80ef9d128f029e9993de0a
Anna A <markova29@yandex.ru>
2023-09-04 13:43:31 +0300

    2 ver

--- src/main/java/StatisticsService.java
+++ src/main/java/StatisticsService.java
@@ -1,6 +1,7 @@
 public class StatisticsService {
     public long findMax(long[] incomes) {
-        long currentMax = incomes[0];
+
+        long currentMax = incomes [0];
         for(long income : incomes) {
             if(currentMax < income) {
                 currentMax = income;
--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -3,14 +3,53 @@ import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
-
     void findMax() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
 
     }
 
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 12, 11, 12,10};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMax3() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {2, 10, 11, 10, 5, 6, 8, 9, 11};
+        long expected = 11;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMax4() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {2, 10, 11, 5, 12, 6, 8, 9, 11};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void findMax5() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {2, 10, 11, 5, 4, 6, 8, 9, 12, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
+
